780a0e2ab1d7ecbf08c61b73ca6b6e22f1c2801d
Zeni <147077236+Zen-ny56@users.noreply.github.com>
2024-01-19 19:13:01 +0000

    00

--- compare.c
+++ compare.c
@@ -12,6 +12,19 @@ int avai_fork(t_philo *philo)
 	return (1);
 }
 
+// int	wash_fork(t_philo *philo)
+// {
+// 	pthread_mutex_lock(&philo->p_data);
+// 	if ((philo->rflag == philo->id || philo->rflag == philo->id)
+// 		&& philo->meals_eaten > 0)
+// 	{
+// 		pthread_mutex_unlock(&philo->p_data);
+// 		return (1);
+// 	}
+// 	pthread_mutex_unlock(&philo->p_data);
+// 	return (0);
+// }
+
 void	set_cutlery(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->p_data);
@@ -25,4 +38,15 @@ void	second_linen(t_philo *philo)
 	if (philo->info->dead_id == philo->id)
 		timeofdeath(philo);
 	pthread_mutex_unlock(&philo->info->lock);
+}
+
+int	satisfied(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->p_data);
+	if (philo->full == true)
+	{
+		pthread_mutex_unlock(&philo->p_data);
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->p_data);
 }
